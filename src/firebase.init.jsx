85b8c7b428a6accf89427d07ba81ae7ba59a1fa6
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-14 11:12:49 +0600

    init file setup

---
+++ src/firebase.init.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX2L3EwEWjzGhjHH7dJf-HYW6IsTmDr90",
+  authDomain: "github-sign-in-f5aef.firebaseapp.com",
+  projectId: "github-sign-in-f5aef",
+  storageBucket: "github-sign-in-f5aef.appspot.com",
+  messagingSenderId: "784773024542",
+  appId: "1:784773024542:web:75f89f5b29e801f1d7170f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
